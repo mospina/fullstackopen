9c0bc26ac144fdd00ab4ea96172c02ed757d75c3
Manuel Ospina <manuel@nativshark.com>
2020-12-25 12:16:42 +0900

    Implement exercises for anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({})  
+
+  const anecdotesCount = props.anecdotes.length
+
+  const handleVoteClick = () => {
+    const newVotes = {...votes, [selected]: votes[selected] ? votes[selected] + 1 : 1 }
+    setVotes(newVotes)
+  }
+
+  const handleNextAnecdoteClick = () => {
+    const selection = getRandomInt(anecdotesCount)
+    setSelected(selection)
+  }  
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>  
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected] ? votes[selected] : 0} votes</p>
+      <Button handleClick={handleVoteClick} text={"vote"} />
+      <Button handleClick={handleNextAnecdoteClick} text={"next anecdote"} />
+      <MostPopular votes={votes} anecdotes={props.anecdotes} />
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const MostPopular = ({votes, anecdotes}) => {
+  const mostPopular = Object.entries(votes).reduce((acc, vote) => {
+    if (vote[1] > acc[1]) {
+     return vote
+    } else {
+     return acc
+    }
+  }, [null, null])
+
+  return (
+    <div>
+      <h2>Anecdote with most votes</h2>  
+      <p>{mostPopular[0] ? anecdotes[mostPopular[0]] : "No votes yet"} </p>
+    </div>
+  )
+}
+
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
