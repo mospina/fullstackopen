53e9e81eff53d6fc22fe9fe4d4b3171d7cf85878
Manuel Ospina <manuel@nativshark.com>
2020-12-25 10:32:51 +0900

    Modify total to use a local state

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -9,28 +9,27 @@ const Button = ({handleClick, text}) => (
 
 const Statistic = ({text, value}) => <tr><td>{text}</td><td>{value}</td></tr>
 
-const Statistics = ({good, neutral, bad}) => {
-  const all = good + neutral + bad
+const Statistics = ({good, neutral, bad, total}) => {
 
   const fixedFloat = (digits, value) => Number.parseFloat(value).toFixed(digits)
 
   return (
     <div>
       <h2>Statistics</h2>
-      { all > 0 ?
+      { total > 0 ?
         <table>
           <tbody>
             <Statistic text={"good"} value={good} />
             <Statistic text={"neutral"} value={neutral} />
             <Statistic text={"bad"} value={bad} />
-            <Statistic text={"all"} value={all} />
+            <Statistic text={"all"} value={total} />
             <Statistic 
               text={"average"} 
-              value={fixedFloat(1, (good - bad) / all) } 
+              value={fixedFloat(1, (good - bad) / total) } 
             />
             <Statistic 
               text={"positive"} 
-              value={`${fixedFloat(1, (good / all) * 100)} %`} 
+              value={`${fixedFloat(1, (good / total) * 100)} %`} 
             />
           </tbody>
         </table>
@@ -41,12 +40,15 @@ const Statistics = ({good, neutral, bad}) => {
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const handleClick = (current, handler) => () => handler(current + 1)
+  const handleClick = (current, handler) => () => {
+    handler(current + 1)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
@@ -54,7 +56,7 @@ const App = () => {
       <Button handleClick={handleClick(good, setGood)} text={"good"} />
       <Button handleClick={handleClick(neutral, setNeutral)} text={"neutral"} />
       <Button handleClick={handleClick(bad, setBad)} text={"bad"} />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
     </div>
   )
 }
