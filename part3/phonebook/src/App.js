5d3792863dcebc947c6edcc225dfe994d70e9b51
Manuel Ospina <manuel@nativshark.com>
2021-01-15 10:43:18 +0900

    Fix error with delete person

--- part3/phonebook/src/App.js
+++ part3/phonebook/src/App.js
@@ -90,7 +90,7 @@ const App = () => {
     }
 
     personServices.remove(person.id).then((status) => {
-      if (status === 200) {
+      if (status === 204) {
         const newPersons = persons.filter((p) => p.id !== person.id);
         setPersons(newPersons);
         flashMessage({ message: `Deleted ${person.name}`, level: "info" });
